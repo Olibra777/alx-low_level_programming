dc33ef97073e2244e46956fa3b4d8d6be52d4258
olibra777 <royrolls81@gmail.com>
2022-09-14 09:31:52 -0700

    My nested loop functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
-
 /**
  * prints_alphabet - entry point func
  *
  * Description: prints the alpabet, in lower case
  * Return: 0
  */
-
 void print_alphabet(void)
 {
 	char c = 'a';
@@ -14,9 +12,7 @@ void print_alphabet(void)
 	while (c <= 'z')
 	{
 		_putchar(c);
-		ch++;
-
+		c++;
 	}
 	_putchar('\n');
-
 }
