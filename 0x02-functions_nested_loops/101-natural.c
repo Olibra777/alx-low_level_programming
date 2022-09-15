9e20f87b1da29906ab881f201fd27b1075fb3fbc
olibra777 <royrolls81@gmail.com>
2022-09-14 17:08:46 -0700

    My nested loop task

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,24 +7,13 @@
  */
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
+	int add, n;
 
-	sum3 = 0;
-	sum5 = 0;
-	sum - 0;
-
-	for (i = 0; i < 1024; ++i)
+	for (n = 0; n < 1024; ++n)
 	{
-		if ((i % 3) == 0)
-		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
-		}
+		if ((n % 3 == 0) || (n % 5 == 0))
+		add += n;
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
