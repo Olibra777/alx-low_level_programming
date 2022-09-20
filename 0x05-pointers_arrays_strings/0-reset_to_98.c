c4cbfa02a382cecef27ce130fa0bba176b58ff59
olibra777 <royrolls81@gmail.com>
2022-09-19 20:06:19 -0700

    Task completed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
-/**
- *  * reset_to_98 - function to updates the value of n to 98
+/** 
+ * reset_to_98 - function to updates the value of n to 98
  * @n: integer pointer to var
  * Return: 0
  */
